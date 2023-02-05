6e7955ae5f32e709401f6310f89712a56009c125
LegendsNeverDl3 <1042154@apps.nsd.org>
2023-02-05 13:35:28 -0800

    massive update

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 window.onload = function(){
-    sessionStorage.setItem("items", 0);
     document.getElementById("back").onclick = goBack;
     document.getElementById("openselect").onclick = selected;
     document.getElementById("openall").onclick = allSelected;
+    //chrome.storage.sync.get("listwebs", printLinks(data));
 }
 
 function goBack(){
@@ -39,4 +39,32 @@ function minusSpan(str){
    
     
 }
+function printLinks(data){
+    for(var i = 0; i < data.length; i++){
+        let spanElement = document.createElement("span");
+        let inputElement = document.createElement("input");
+        let labelElement = document.createElement("label");
+        let divElement = document.createElement("div");
+        let brElement = document.createElement("br");
+
+        spanElement.id = data[i];
+        inputElement.type = "checkbox";
+        inputElement.id = i;
+        inputElement.className = "check";
+        labelElement.htmlFor = "i";
+        labelElement.className = "links";
+        labelElement.textContent = data[i];
+        divElement.className = "delete";
+        divElement.id = data[i];
+
+        document.body.appendChild(spanElement);
+        spanElement.prepend(inputElement);
+        spanElement.appendChild(labelElement);
+        labelElement.appendChild(divElement);
+        document.body.appendChild(brElement);
+
+    }
+    
+}
+
 
