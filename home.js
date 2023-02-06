67d58c9ef988da3a5f93b83152f494b332f0e774
LegendsNeverDl3 <1042154@apps.nsd.org>
2023-02-05 16:39:09 -0800

    fixed homepage settings issue

--- home.js
+++ home.js
@@ -1,8 +1,13 @@
 window.onload = function(){
     document.getElementById("continue").onclick =switchs;
+    document.getElementById("settings").onclick=openOptions;
     
 }
 function switchs(){
     location.href = "popup.html";
 
+}
+// a function to open settings
+function openOptions() { 
+    window.open("options.html");
 }
