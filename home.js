13f19530c45ab3f6d9fe2e039ae54dd1499f87cf
LegendsNeverDl3 <1042154@apps.nsd.org>
2023-09-28 14:46:30 -0700

    Revert "Arragned files so that things are nicer"
    This reverts commit 73a49dd11b273bf0f9567b78c981eb76869c2a43.

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-    document.getElementById("continue").onclick = switchs;
-    document.getElementById("settings").onclick = openOptions;
+    document.getElementById("continue").onclick =switchs;
+    document.getElementById("settings").onclick=openOptions;
     
 }
 function switchs(){
